f7b605b8d5b48d16e5fe897912c0628b4328485d
Romilly Cocking <romilly.cocking@gmail.com>
2025-02-24 09:17:25 +0000

    Simple version working

---
+++ docs/problem.md
@@ -0,0 +1,1 @@
+Number of readable PDF files: 14053
---
+++ src/ollama_oracle/ollama_oracle.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+import time
+
+from pdfminer.high_level import extract_text
+from pydantic import BaseModel
+import sqlite3
+from ollama import Client
+
+TEMPLATE = "Can you tell me the title and author from this start of an academic paper?{text}"
+
+
+class Paper(BaseModel):
+    title: str
+    authors: list[str]
+
+
+PAPER_FORMAT = Paper.model_json_schema()
+
+
+def find_paper(path: str) -> Paper:
+    text = extract_text(path, maxpages=1)
+    messages = [{
+        "role": "user",
+        "content": TEMPLATE.format(text=text)
+    }]
+    client = Client(host= "http://localhost:11434")
+    response = client.chat('qwen2.5:14b', messages=messages, format=PAPER_FORMAT)
+    return Paper.model_validate_json(response.message.content)
+
+
+class Library:
+
+    def __init__(self):
+        self.connection = sqlite3.connect('../../pdf_files.db')
+        self._create_table()
+
+    def __del__(self):
+        self.connection.close()
+
+    def update_paper(self, path: str):
+        paper = find_paper(path)
+        sql = """
+        INSERT OR REPLACE INTO pdf_info (path, title, authors)
+        VALUES (?, ?, ?)
+
+        """
+        cursor = self.connection.cursor()
+        params = (path, paper.title, ', '.join(paper.authors))
+        print(params)
+        cursor.execute(sql, params)
+        self.connection.commit()
+
+    def _create_table(self):
+        """Create the pdf_info table if it does not exist."""
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS pdf_info (
+            path TEXT PRIMARY KEY,
+            title TEXT,
+            authors TEXT,
+            UNIQUE(title)
+        );
+        """
+        cursor = self.connection.cursor()
+        cursor.execute(create_table_query)
+        self.connection.commit()
+
+
+def process_files(*file_list: str):
+    library = Library()
+    for path in file_list:
+        library.update_paper(path.strip())
+
+import os
+
+
+def pdfs_in(directory: str):
+    pdf_files = []
+    # Walk through the given directory and subdirectories
+    for root, _, files in os.walk(directory):
+        for file in files:
+            # Check if file has a .pdf extension (case-insensitive)
+            if file.lower().endswith('.pdf'):
+                # Add the absolute path of the PDF to the list
+                pdf_files.append(os.path.abspath(os.path.join(root, file)))
+    return pdf_files
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    files = pdfs_in('../../data/pdfs')
+    print(len(files))
+    process_files(*files)
+    end_time = time.time()
+    print(f"\nTime taken: {end_time - start_time:.2f} seconds")
